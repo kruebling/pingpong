8cfb9df36a213d6f6a197db0642af631e700ae56
kr <ktrhx4@mail.missouri.edu>
2017-08-18 12:01:57 -0700

    add backend logic to determine remainder of each index, add front end logic to push final array to a list

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,31 @@
+//Back-End
 var nArray = [];
 
-function split(input) {
+function inputArray(input) {
   for (i = 0; i <= input; i ++)
-    if (i <= input) {
-      nArray.push(i);
-    }
+  if (i <= input) {
+    nArray.push(i);
+  }
 }
+
+function ping(arr) {
+  for (i = 1; i <= arr.length; i++)
+  if (i % 3 === 0)
+  arr[i] = "ping";
+}
+
+function pong(arr) {
+  for (i = 1; i <= arr.length; i++)
+  if (i % 5 === 0)
+  arr[i] = "pong";
+}
+
+function pingPong(arr) {
+  for (i = 1; i <= arr.length; i++)
+  if (i % 5 === 0 && i % 3 === 0)
+  arr[i] = "ping-pong";
+}
+
 
 //Front-End
 $(document).ready(function() {
@@ -13,7 +33,14 @@ $(document).ready(function() {
     event.preventDefault();
 
       var n = $("input#userInput").val();
-      split(n);
-      alert(nArray);
+      inputArray(n);
+      ping(nArray);
+      pong(nArray);
+      pingPong(nArray);
+
+      $listSelector = $("#answer");
+      $.each(nArray, function(i, obj) {
+        $('ul').append($('<li></li>').html(nArray[i]));
+    });
   });
 });
